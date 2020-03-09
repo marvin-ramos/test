c11343989df4ab19bc5fc0bb3e3f6f1ee909fed6
ramosmarvin50@gmail.com <ramosmarvin50@gmail.com>
2020-03-09 19:15:20 +0800

    revise

--- problem1.php
+++ problem1.php
@@ -1,5 +1,15 @@
 <?php
-$name_list = array('Ava', 'Emma', 'Olivia','Olivia', 'Sophia', 'Emma');
-$result = array_unique($name_list);
-print_r($result);
+// $name_list = array('Ava', 'Emma', 'Olivia','Olivia', 'Sophia', 'Emma');
+// $result = array_unique($name_list);
+// print_r($result);
 
+$array = array(
+    array('Ava', 'Emma', 'Olivia'),
+    array('Olivia', 'Sophia', 'Emma')
+);
+
+function fn($a) {
+    print "$a[0], $a[1], \n";
+}
+
+foreach ($array as $a) fn($a);
